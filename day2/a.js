80a93ac0a9ddcf4d1745a5db2d5e83082a49c9fd
pg541425 <pg541425@gmail.com>
2026-07-31 11:45:52 +0530

    Day2

---
+++ day2/a.js
@@ -0,0 +1,58 @@
+// console.log("===== Variables =====");
+// var text="Something";
+// console.log(text);
+
+// console.log("\n ==== If Else ====");
+// var a=40;
+// var b=30;
+// if(a>b){
+//     console.log("a is greater, value of a = ",a);
+// }else{
+//     console.log("b is greater, value of b = ",b);
+// }
+
+// console.log("\n=====Function=====");
+// function abes(){
+//     console.log("Welcome Students");
+//     console.log("Welcome to Node.js");
+// }
+// abes();
+
+// console.log("=========Function with Parameter==========");
+// function greet(fname, lname){
+//     console.log("Hello",fname,lname);
+// }
+// greet("Utkarsh","Dixit");
+// greet("Waseem","Sir");
+
+// console.log("=====Funtion Return=====");
+// function add(x,y){
+//     return x+y;
+// }
+// var result=add(20,30);
+// console.log("Sum = ",result);
+
+// console.log("\n=======Default arguments=======");
+// function fullName(fname="abes",lname="Engineering College"){
+//     return fname+" "+lname;
+// }
+// console.log(fullName());
+// console.log(fullName("Shivam","Gupta"));
+
+// console.log("\n======Percentage======");
+// function totalMarks(math,english,science){
+//     return math+english+science;
+// }
+// function percentage(total){
+//     return (total/300)*100;
+// }
+// var total=totalMarks(80,75,90);
+// console.log("Total Marks = ",total);
+// console.log("Percentage = ",percentage(total)+"%");
+
+console.log("\n====While Loop=====");
+var i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
