cd1c8c7911e23ceb741f63c903944be2d6de89a8
pg541425 <pg541425@gmail.com>
2026-07-31 10:57:18 +0530

    day1

---
+++ day1/index.js
@@ -0,0 +1,26 @@
+// const app=require('./app.js');
+
+// var a=20;
+// var b=20;
+// var c=20;
+// console.log(a+b+c);
+
+// var x=20;
+// if(x==5){
+//     console.log("x is 20");
+// }else{
+//     console.log("Invalid error");
+// }
+
+// for(var i=0; i<5;i++){
+//     console.log(i);
+// }
+
+// var x=20;
+// const arr=[1,2,3,4,5];
+// for(var i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }console.log(arr[2]);
+
+// console.log(app.xz);
+// console.log(app.yy);
