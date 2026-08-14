9af32a149e7c6a7b3b7b7f14967f0e3f86659e40
pg541425 <pg541425@gmail.com>
2026-08-14 11:56:13 +0530

    task 8 with display function in object

---
+++ day3/task8.js
@@ -0,0 +1,10 @@
+const employee ={
+    id:101,
+    name:"Shivam",
+    department:"CSE",
+    display:function(){
+        console.log("ID = ",this.id);
+        console.log("Name = ",this.name);
+        console.log("Department = ",this.department);
+    }
+};
