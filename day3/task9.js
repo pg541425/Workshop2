c93b93d4e39a1f440a0ca44d27b7bda8d4d73bda
pg541425 <pg541425@gmail.com>
2026-08-14 12:03:12 +0530

    add or delete key in object

---
+++ day3/task9.js
@@ -0,0 +1,10 @@
+//add or delete key in object
+var student={
+    id:184,
+    name:"Shivam",
+    course:"B.Tech"
+};
+student.age=21;
+console.log(student);
+delete student.course;
+console.log(student);
