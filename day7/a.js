6102cc4e820e7bfa3c96966e00fc6757464ae5d5
pg541425 <pg541425@gmail.com>
2026-09-21 15:45:09 +0530

    Corrected codes and files

--- day7/a.js
+++ day7/a.js
@@ -19,29 +19,29 @@
 // }
 // let obj2=new hello1("Shivam");
 
-class student {
-    name;
-    roll_no;
-    city;
-    id;
-    constructor(name, roll_no, city, id) {
-        this.name = name;
-        this.roll_no = roll_no;
-        this.city = city;
-        this.id = id;
-        console.log("Name : " + this.name);
-        console.log("Roll no. : " + this.roll_no);
-        console.log("City : " + this.city);
-        console.log("Id : " + this.id);
-    }
-}
-let s = new student("Shivam", 2503215300184, "UK", 101);
-let s1 = new student("Priya", 2503215300185, "Delhi", 102);
-let s2 = new student("Rahul", 2503215300186, "Mumbai", 103);
-let s3 = new student("Anjali", 2503215300187, "Bangalore", 104);
-let s4 = new student("Amit", 2503215300188, "Lucknow", 105);
-console.log(s);
-console.log(s1);
-console.log(s2);
-console.log(s3);
-console.log(s4);
+// class student {
+//     name;
+//     roll_no;
+//     city;
+//     id;
+//     constructor(name, roll_no, city, id) {
+//         this.name = name;
+//         this.roll_no = roll_no;
+//         this.city = city;
+//         this.id = id;
+//         console.log("Name : " + this.name);
+//         console.log("Roll no. : " + this.roll_no);
+//         console.log("City : " + this.city);
+//         console.log("Id : " + this.id);
+//     }
+// }
+// let s = new student("Shivam", 2503215300184, "UK", 101);
+// let s1 = new student("Priya", 2503215300185, "Delhi", 102);
+// let s2 = new student("Rahul", 2503215300186, "Mumbai", 103);
+// let s3 = new student("Anjali", 2503215300187, "Bangalore", 104);
+// let s4 = new student("Amit", 2503215300188, "Lucknow", 105);
+// console.log(s);
+// console.log(s1);
+// console.log(s2);
+// console.log(s3);
+// console.log(s4);
--- day8/a.js
+++ day8/a.js
@@ -1,28 +1,80 @@
-//Inheritance
-class Employee{
-    constructor(id,name,basicSalary){
-        this.name=name;
-        this.id=id;
-        this.basicSalary=basicSalary;
-    }
-    calculateSalary(basicSalary){
-        return this.basicSalary;
-    }
+// //Inheritance
+// class Employee{
+//     constructor(id,name,basicSalary){
+//         this.name=name;
+//         this.id=id;
+//         this.basicSalary=basicSalary;
+//     }
+//     calculateSalary(basicSalary){
+//         return this.basicSalary;
+//     }
+// }
+
+// //Derived Class
+// class Manager extends Employee{
+//     constructor(id,name,basicSalary,incentive){
+//     super(id,name,basicSalary);
+//     this.incentive=incentive;
+//     }
+
+//     calculateSalary(){
+//         return this.basicSalary+this.incentive;
+//     }
+// }
+// let emp1=new Employee(101,"Shivam",12000);
+// console.log(emp1.calculateSalary());
+
+// let emp2=new Manager(101,"Shatakshi",15000,3000);
+// console.log(emp2.calculateSalary());
+
+
+// //Call Back Function
+// function greeUser(name,callback){
+//     console.log("Hello "+name);
+//     callback();
+// }
+
+// greeUser("Bob", function(){
+//     console.log("Callback function executed.");
+// });
+
+// console.log("Start ")
+// setTimeout(function(){
+//     console.log("Task Completed")
+// },2000)
+
+// console.log("End")
+
+
+// //Synchronus
+
+// console.log(10)
+// console.log(20)
+// console.log(40)
+// console.log(50)
+
+// //Async
+
+// console.log("Start");
+
+// setTimeout(function(){
+//     console.log("Middle");
+// }, 2000);
+// console.log("End");
+
+//other Example
+
+function printName(){
+    console.log("Shivam")
 }
+printName();
+console.log("Welcome");
 
-//Derived Class
-class Manager extends Employee{
-    constructor(id,name,basicSalary,incentive){
-    super(id,name,basicSalary);
-    this.incentive=incentive;
-    }
-
-    calculateSalary(){
-        return this.basicSalary+this.incentive;
-    }
+function printName(){
+    setTimeout(()=>{
+        console.log("Shivam");
+    },2000);
 }
-let emp1=new Employee(101,"Shivam",12000);
-console.log(emp1.calculateSalary());
+printName();
 
-let emp2=new Manager(101,"Shatakshi",15000,3000);
-console.log(emp2.calculateSalary());
+console.log("Welcome");
